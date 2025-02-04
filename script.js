09049c8c85ee636bff9a8920218a4caac9e5ab45
Carlos Teixeira <c.texeirajr@gmail.com>
2025-02-04 09:13:48 -0300

    improve
    + correção de impressão

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const btnGenerate = document.querySelector('#generatePdf');
+const btnGenerate = document.getElementById('generatePdf');
 
-btnGenerate.addEventListener('click', () =>{
-    //conteúdo do PDF
-    const content = document.querySelector("#content");
-
-    //configuração do conteúdo final do PDF
-    const options = {
-        margin:[30,0,0,0],
-        filename: "arquivo.pdf",
-        html2canvas: {scale: 2},    
-        jsPDF:{format: "letter", orientation: "portrait"},
-    }
+function printScreen(){
+    btnGenerate.style.display = 'none';
+    window.print();    
     
-    //gerar e baixar o pdf
-    html2pdf().set(options).from(content).save();
-});
+}
